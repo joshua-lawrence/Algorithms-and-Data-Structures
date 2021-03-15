f946c102d89129cb8da177a4c1905b1fc3de16c3
Joshua Lawrence <joshua98lawrence@gmail.com>
2021-03-14 20:08:27 -0600

    Create BubbleSort.java

---
+++ src/algorithms/BubbleSort.java
@@ -0,0 +1,16 @@
+package algorithms;
+
+public class BubbleSort {
+	
+	//sweep through array, compare n to n+1, if n+1 is less than n, swap
+	
+	void sort(int arr[]) {
+		int n = arr.length;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
