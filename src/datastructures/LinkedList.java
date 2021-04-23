180fc0f8ee57a218f86f8bd48ecdb467693c153a
joshua.lawrence <joshua98lawrence@gmail.com>
2021-04-23 00:17:21 -0600

    built basic stack

--- src/datastructures/LinkedList.java
+++ src/datastructures/LinkedList.java
@@ -1,22 +1,26 @@
 package datastructures;
 
-public class LinkedList {
+public class LinkedList<T> {
 
-    private Node head;
+    private Node<T> head;
 
-    LinkedList(Node head) {
+    LinkedList(Node<T> head) {
         this.head = head;
+    }
+
+    LinkedList(T head) {
+        this.head = new Node<>(head);
     }
 
     private static class Node<T> {
         private T data;
-        private Node next;
+        private Node<T> next;
 
         Node(T data) {
             this.data = data;
         }
 
-        Node(T data, Node next) {
+        Node(T data, Node<T> next) {
             this.data = data;
             this.next = next;
         }
@@ -29,25 +33,44 @@ public class LinkedList {
             this.data = data;
         }
 
-        public Node getNext() {
+        public Node<T> getNext() {
             return next;
         }
 
-        public void setNext(Node next) {
+        public void setNext(Node<T> next) {
             this.next = next;
         }
+        public void setNext(T next) {
+            this.next = new Node<>(next);
+        }
+
+        public void deleteNext() {
+            this.next = null;
+        }
+
     }
 
-    public void push(Node node) {
-        Node current = this.head;
+    public void push(T value) {
+        Node<T> current = this.head;
         while(current.getNext() != null) {
             current = current.getNext();
         }
-        current.setNext(node);
+        current.setNext(new Node<T>(value));
+    }
+
+    public T pop() {
+        Node<T> current = this.head;
+        Node<T> previous = null;
+        while(current.getNext() != null) {
+            previous = current;
+            current = current.getNext();
+        }
+        previous.deleteNext();
+        return current.getData();
     }
 
     public void printList() {
-        Node current = this.head;
+        Node<T> current = this.head;
         while(current.getNext() != null) {
             System.out.println(current.data);
             current = current.getNext();
@@ -56,11 +79,11 @@ public class LinkedList {
     }
 
     public static void main(String[] args) {
-        Node newNode = new Node<Integer>(12);
-        LinkedList linkedList = new LinkedList(newNode);
-        newNode.setNext(new Node<Integer>(14));
+        Node<Integer> newNode = new Node<>(12);
+        LinkedList<Integer> linkedList = new LinkedList<>(newNode);
+        newNode.setNext(14);
 
-        linkedList.push(new Node<Double>(42.5));
+        linkedList.push(42);
 
         linkedList.printList();
 
--- src/datastructures/Stack.java
+++ src/datastructures/Stack.java
@@ -2,6 +2,50 @@ package datastructures;
 
 
 //Can create a stack with either an array or a linked list.
-public class Stack {
+public class Stack<T> {
 
+    private Integer count = 0;
+    LinkedList<T> list;
+
+    Stack(T value) {
+        this.list = new LinkedList<T>(value);
+    }
+
+    public void push(T value) {
+        setCount(count + 1);
+        list.push(value);
+    }
+
+    public T pop() {
+        setCount(count - 1);
+        return list.pop();
+    }
+
+    public void empty() {
+        while(this.getCount() != 0) {
+            System.out.println(this.pop());
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack(12);
+
+        stack.push(12);
+        stack.push(15);
+        stack.push(24);
+        stack.push(54);
+
+        stack.empty();
+
+    }
+
+
+    public Integer getCount() {
+        return count;
+    }
+
+    private void setCount(Integer count) {
+        this.count = count;
+    }
 }
